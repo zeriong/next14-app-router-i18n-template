97872e29ffc24f1b4dc21cb2db8c52b2b7d0e42c
jaery <jaeryong95@gmail.com>
2024-08-25 16:30:36 +0900

    refactor: main 페이지로 이동 시 a태그를 활용하여 페이지를 reload하며 번역정보가 재실행되어 적용될 수 있도록 설정

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,21 +1,26 @@
+import { cookies } from 'next/headers';
 import React from 'react';
-import getTranslation from "@/libs/i18n/utils/getTranslation";
-import {Locale} from "@/libs/i18n";
-import Link from "next/link";
-import {cookies} from "next/headers";
-import {LOCALE_COOKIE} from "@/constants/common";
+
+import { LOCALE_COOKIE } from '@/constants/common';
+import { Locale } from '@/libs/i18n';
+import getTranslation from '@/libs/i18n/utils/getTranslation';
 
 export default async function Page({ params }: { params: { lng: Locale } }) {
-    const t = await getTranslation((cookies().get(LOCALE_COOKIE)?.value || params.lng) as Locale);
-    return (
-          <div>
-              <div>
-                  <h1>{t("Hello world!")}</h1>
-                  <p>{t("It implemented i18n without using a library.")}</p>
-              </div>
-              <Link href="/main" className="block p-[16px] bg-black text-white mt-[16px] ml-[16px] w-fit">
-                  {t("Link to Main")}
-              </Link>
-          </div>
+  const t = await getTranslation(
+    (cookies().get(LOCALE_COOKIE)?.value || params.lng) as Locale,
+  );
+  return (
+    <div>
+      <div>
+        <h1>{t('Hello world!')}</h1>
+        <p>{t('It implemented i18n without using a library.')}</p>
+      </div>
+      <a
+        href="/main"
+        className="block p-[16px] bg-black text-white mt-[16px] ml-[16px] w-fit"
+      >
+        {t('Link to Main')}
+      </a>
+    </div>
   );
 }
