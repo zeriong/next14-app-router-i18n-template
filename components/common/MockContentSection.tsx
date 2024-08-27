74b77c67e57b2a25eb292b9b1d4a8f99ebc014e2
jaery <jaeryong95@gmail.com>
2024-08-27 13:09:27 +0900

    feat: 더미 컨텐츠 컴포넌트 생성

---
+++ components/common/MockContentSection.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function MockContentSection() {
+  return (
+    <div>
+      <section className="bg-red-400 flex justify-center items-center font-bold text-[40px] py-[300px]">
+        It's Dummy
+      </section>
+      <section className="bg-blue-400 flex justify-center items-center font-bold text-[40px] py-[300px]">
+        It's Dummy
+      </section>
+      <section className="bg-gray-400 flex justify-center items-center font-bold text-[40px] py-[300px]">
+        It's Dummy
+      </section>
+      <section className="bg-green-400 flex justify-center items-center font-bold text-[40px] py-[300px]">
+        It's Dummy
+      </section>
+    </div>
+  );
+}
